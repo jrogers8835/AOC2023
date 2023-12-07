55f31b33130b3877708424c6bf7d1f72f32f3c58
jrogers8835 <jrogers8835@gmail.com>
2023-12-07 16:57:25 -0500

    Day 7 part 1
    part 2 only works for the sample

---
+++ D07/Day07.py
@@ -0,0 +1,90 @@
+from D07.inputData import inputObj
+
+
+def convert(card):
+    match card:
+        case 'A':
+            return 14
+        case 'K':
+            return 13
+        case 'Q':
+            return 12
+        case 'J':
+            return 1
+        case 'T':
+            return 10
+        case _:
+            return int(card)
+
+
+class Hand:
+    def __init__(self, cards, bid):
+        self.cards = cards
+        self.bid = bid
+
+    def type(self):
+        counts = {}
+        jcount = 0
+        for card in self.cards:
+            if card == 'J':
+                jcount += 1
+                continue
+            if card not in counts:
+                counts[card] = 0
+            counts[card] += 1
+        countTot = [0, 0, 0, 0, 0, 0]
+        for c in counts:
+            countTot[counts[c]] += 1
+        if countTot[5] > 0 or \
+                (countTot[3] > 0 and jcount == 2) or \
+                (countTot[2] > 0 and jcount == 3) or \
+                (jcount > 3):
+            return 7
+        if countTot[4] > 0 or \
+                (countTot[3] > 0 and jcount == 1) or \
+                (countTot[2] > 0 and jcount == 2):
+            return 6
+        if (countTot[3] > 0 and countTot[2] > 0) or \
+                (countTot[2] == 2 and jcount == 1):
+            return 5
+        if countTot[3] > 0 or \
+                (countTot[2] > 0 and jcount == 1):
+            return 4
+        if countTot[2] == 2:
+            return 3
+        if countTot[2] > 0:
+            return 2
+        return 1
+
+    def __str__(self):
+        return f'(cards: {self.cards}, bid: {self.bid})'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __hash__(self):
+        return convert(self.cards[4]) + \
+               convert(self.cards[3]) * 1000 + \
+               convert(self.cards[2]) * 1000000 + \
+               convert(self.cards[1]) * 1000000000 + \
+               convert(self.cards[0]) * 1_000000000000
+
+    def __lt__(self, other):
+        if self.type() == other.type():
+            return self.__hash__() < other.__hash__()
+        else:
+            return self.type() < other.type()
+
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
+    def __gt__(self, other):
+        return not self.__lt__(other) and not self.__eq__(other)
+
+
+def run():
+    vals = list(map(lambda x: x.bid, sorted(map(lambda x: Hand(x[0], x[1]), inputObj))))
+    total = 0
+    for idx, v in enumerate(vals):
+        total += v * (idx + 1)
+    print(total)
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from D02 import Day02
 from D03 import Day03
 from D04 import Day04
 from D05 import Day05
+from D07 import Day07
 
 # Day01.run()
 # Day02.run()
 # Day03.run(2)
 # Day04.run()
-Day05.run()
+# Day05.run()
+Day07.run()
